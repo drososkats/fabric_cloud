8916713c82214a834b8afc81cce2477c3297516e
drososkats <katsimprasdr@gmail.com>
2026-02-03 23:13:04 +0000

    Add Dockerfiles and docker-compose for automated deployment

--- my_files/frontend/src/config.js
+++ my_files/frontend/src/config.js
@@ -1,3 +1,3 @@
 // Use "localhost" or ip address  - works on our laptop 
-export const API_URL = "http://localhost:5000";
+export const API_URL = "http://192.168.1.5:5000";
 export const SERVER_URL = "http://localhost:5000";
